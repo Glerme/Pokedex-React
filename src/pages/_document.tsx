088ad2d54ef9ba44f22be6a58e95fd2314ea8e55
Guilherme Godoi <guiggff@gmail.com>
2021-05-24 14:09:46 -0300

    :wrench: Adicionado o Manifest.json

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -41,7 +41,7 @@ export default class MyDocument extends Document {
           <title>Pokedex</title>
           <link rel="manifest" href="/manifest.json" />
 
-          <link rel="shortcut icon" href="favicon.svg" type="image/x-icon" />
+          <link rel="shortcut icon" href="logo.svg" type="image/x-icon" />
           <meta charSet="utf-8" />
           <meta http-equiv="X-UA-Compatible" content="IE=edge" />
           <meta
@@ -50,24 +50,7 @@ export default class MyDocument extends Document {
           />
           <meta name="description" content="Description" />
           <meta name="keywords" content="Keywords" />
-          <link
-            rel="apple-touch-icon"
-            sizes="180x180"
-            href="/apple-touch-icon.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="32x32"
-            href="/favicon-32x32.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="16x16"
-            href="/favicon-16x16.png"
-          />
-          <link rel="manifest" href="/manifest.json" />
+
           <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
 
           <link rel="preconnect" href="https://fonts.gstatic.com" />
